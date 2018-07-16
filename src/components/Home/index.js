69b7d24a90215b75583898df5200174ca4340821
Eben Palazzolo <olozzalap@gmail.com>
2018-07-16 14:45:10 -0400

    Created State for HomeCOmponent and finally figured out how to communicate with geth private blockchain
    testrpc was also running which caused localhost: 8545 to reach there instead

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,26 +2,56 @@ import React, { Component } from 'react';
 import Web3 from 'web3';
 import './style.css';
 
-var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 class Home extends Component {
 
 	constructor(props) {
 	    super(props);
 	    this.state = {
-	      block_ids: [],
-	      block_hashes: [],
-	      curr_block: null
+            block_ids: [],
+            block_hashes: [],
+            current_accounts: [],
+            current_block: null,
+            coinbase: null
 	    }
-	  }
+	}
+    componentWillMount() {
+        console.log(web3.eth);
+        let accounts, blockNumber;
+        web3.eth.getAccounts().then( (accountsResp) => {
+            accounts = accountsResp;
+            console.log(accounts);
+            this.setState({
+                current_accounts: accounts
+            });
+        }).then( () => {
+            console.log(this.state.current_accounts);
+        });
+        web3.eth.getBlockNumber().then( (blockNumberResp) => {
+            blockNumber = blockNumberResp;
+            console.log(blockNumber);
+            this.setState({
+                current_block: blockNumber
+            });
+        }).then( () => {
+            console.log(this.state.current_block);
+        });
+        web3.eth.getCoinbase().then( (coinbase) => {
+            this.setState({
+                coinbase: coinbase
+            });
+        }).then( () => {
+            console.log(this.state.coinbase);
+        });
+    }
 
     render() {
-    	console.log(web3.eth);
     	console.log("ello");
-    	console.log(web3.eth.getCoinbase());
         return (
             <div className="wonderous-home">
                 <h2>Home page</h2>
+                <p>Current Block: {this.state.current_block}</p>
             </div>
         );
     }
