e7a868981ee31d936675b02d8e478d09a1cd5be1
Eben Palazzolo <olozzalap@gmail.com>
2018-07-16 18:16:15 -0400

    HomeComponent updats to now retrieve latest blocks from state.current_block with an optional max_blocks to limit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
+import _ from 'lodash';
+import { Link } from 'react-router-dom'
+
 import './style.css';
 
 let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
@@ -17,37 +20,56 @@ class Home extends Component {
 	    }
 	}
     componentWillMount() {
-        console.log(web3.eth);
-        let accounts, blockNumber;
+        // Retrieves current accounts
         web3.eth.getAccounts().then( (accountsResp) => {
-            accounts = accountsResp;
-            console.log(accounts);
             this.setState({
-                current_accounts: accounts
+                current_accounts: accountsResp
             });
         }).then( () => {
             console.log(this.state.current_accounts);
         });
-        web3.eth.getBlockNumber().then( (blockNumberResp) => {
-            blockNumber = blockNumberResp;
-            console.log(blockNumber);
-            this.setState({
-                current_block: blockNumber
-            });
-        }).then( () => {
-            console.log(this.state.current_block);
-        });
+        // Retrieves blockchain coinbase
         web3.eth.getCoinbase().then( (coinbase) => {
             this.setState({
                 coinbase: coinbase
             });
         }).then( () => {
             console.log(this.state.coinbase);
+            console.log(web3.eth);
+        });
+        // Retrieves current latest block number, then runs getBlocks to get the latest Blocks counting down from state.current_block
+        web3.eth.getBlockNumber().then( (blockNumberResp) => {
+            this.setState({
+                current_block: blockNumberResp
+            });
+        }).then( () => {
+            console.log(this.state.current_block);
+            this.getBlocks(this.state.current_block, 20);
         });
     }
 
+    getBlocks(current_block_num, max_blocks = 10) {
+        const block_ids = this.state.block_ids.slice();
+        const block_hashes = this.state.block_hashes.slice();
+
+        if (current_block_num < max_blocks) max_blocks = current_block_num;
+        for (var i = 0; i < max_blocks; i++, current_block_num--) {
+            web3.eth.getBlock(current_block_num).then((currentBlockObj) => {
+                console.log(currentBlockObj);
+                block_ids.push(currentBlockObj.number);
+                block_hashes.push(currentBlockObj.hash);
+            });
+        }
+        this.setState({
+            block_ids: block_ids,
+            block_hashes: block_hashes
+        })
+        console.log(this.state.block_ids);
+        console.log(this.state.block_hashes);
+    }
+
+
     render() {
-    	console.log("ello");
         return (
             <div className="wonderous-home">
                 <h2>Home page</h2>
